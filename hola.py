1550fa57e986bd755a009b024e3df8173d9cc4bb
semaforo0223 <riosb1999@gmail.com>
2025-04-24 10:39:06 -0500

    prueba 2

--- hola.py
+++ hola.py
@@ -1,11 +1,11 @@
 from machine import Pin
 from time import sleep
-
+#andres makina
 
 LED = Pin(2, Pin.OUT)
 
 while True:
     LED.value(1)
-    sleep(1)
+    sleep(0.5)
     LED.value(0)
-    sleep(1)
+    sleep(2)
