caf66ef99d45d17ba901d1b67d966f651e9ff7eb
semaforo0223 <riosb1999@gmail.com>
2025-04-24 10:35:28 -0500

    TOUCHPAD

--- hola.py
+++ hola.py
@@ -1,11 +1,13 @@
-from machine import Pin
+from machine import Pin, TouchPad
 from time import sleep
 
-
+touchPrueba = TouchPad(15)
 LED = Pin(2, Pin.OUT)
 
 while True:
+    touchPrueba.read()
     LED.value(1)
     sleep(1)
     LED.value(0)
     sleep(1)
+    print(touchPrueba.read())
